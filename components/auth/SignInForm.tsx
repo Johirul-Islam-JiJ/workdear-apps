0e32faeba2898adf236e03e585364764073912f4
Md Iqbal Hossen <iqbalhossen60483@gmail.com>
2025-09-07 15:46:36 +0600

    login screen design done

--- components/auth/SignInForm.tsx
+++ components/auth/SignInForm.tsx
@@ -1,18 +1,49 @@
 import { useThemeColor } from "@/hooks/useThemeColor";
+import { Fontisto } from "@expo/vector-icons";
 import Feather from "@expo/vector-icons/Feather";
-import Fontisto from "@expo/vector-icons/Fontisto";
+import { yupResolver } from "@hookform/resolvers/yup";
 import { Link } from "expo-router";
 import React from "react";
+import { Controller, useForm } from "react-hook-form";
 import { View } from "react-native";
+import * as yup from "yup";
 import Button from "../libs/Button";
 import Input from "../libs/Input";
 import { ThemedText } from "../libs/ThemedText";
 import { ThemedView } from "../libs/ThemedView";
 
+const schema = yup.object().shape({
+  email: yup.string().email().required(),
+  password: yup.string().required(),
+});
+
+type Payload = {
+  email: string;
+  password: string;
+  device_name?: string;
+};
+
 const SignInForm = () => {
   const [showPasswoard, setShowPassword] = React.useState(false);
-  const error = false;
-  const iconColor = useThemeColor(error ? "error" : "placeHolder");
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Payload>({
+    resolver: yupResolver(schema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+  const emailIconColor = useThemeColor(errors.email ? "error" : "placeHolder");
+  const passwordIconColor = useThemeColor(
+    errors.password ? "error" : "placeHolder"
+  );
+
+  async function onSubmit(data: Payload) {
+    console.log(data);
+  }
 
   return (
     <ThemedView
@@ -27,28 +58,55 @@ const SignInForm = () => {
       <View style={{ gap: 7 }}>
         <View>
           <ThemedText>Email</ThemedText>
-          <Input
-            placeholder="Enter your email"
-            startIcon={<Fontisto name="email" size={20} color={iconColor} />}
+          <Controller
+            control={control}
+            name="email"
+            render={({ field: { onChange, value } }) => (
+              <Input
+                placeholder="Enter your email"
+                value={value}
+                onChangeText={onChange}
+                error={errors.email?.message}
+                startIcon={
+                  <Fontisto name="email" size={20} color={emailIconColor} />
+                }
+              />
+            )}
           />
         </View>
         <View>
           <ThemedText>Password</ThemedText>
-          <Input
-            placeholder="Enter the password"
-            secureTextEntry={!showPasswoard}
-            startIcon={<Feather name="lock" size={20} color={iconColor} />}
-            endIcon={
-              <Feather
-                onPress={() => setShowPassword(!showPasswoard)}
-                name={showPasswoard ? "eye-off" : "eye"}
-                size={20}
-                color={iconColor}
+          <Controller
+            name="password"
+            control={control}
+            render={({ field: { onChange, value } }) => (
+              <Input
+                placeholder="Enter the password"
+                secureTextEntry={!showPasswoard}
+                error={errors.password?.message}
+                value={value}
+                onChangeText={onChange}
+                startIcon={
+                  <Feather name="lock" size={20} color={passwordIconColor} />
+                }
+                endIcon={
+                  <Feather
+                    onPress={() => setShowPassword(!showPasswoard)}
+                    name={showPasswoard ? "eye-off" : "eye"}
+                    size={20}
+                    color={passwordIconColor}
+                  />
+                }
               />
-            }
+            )}
           />
         </View>
-        <Button title="Sign in" variant="Contained" />
+        <Button
+          onPress={handleSubmit(onSubmit)}
+          title="Sign in"
+          variant="Contained"
+          style={{ marginTop: 10 }}
+        />
       </View>
 
       <View
