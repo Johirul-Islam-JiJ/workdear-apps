08c4e30b8f1a91ae35ff9086168558eb07b0194f
Md Iqbal Hossen <iqbalhossen60483@gmail.com>
2025-09-07 12:46:27 +0600

    color pallete and icons  added

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,26 +1,30 @@
-/**
- * Below are the colors that are used in the app. The colors are defined in the light and dark mode.
- * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
- */
-
-const tintColorLight = '#0a7ea4';
-const tintColorDark = '#fff';
-
 export const Colors = {
   light: {
-    text: '#11181C',
-    background: '#fff',
-    tint: tintColorLight,
-    icon: '#687076',
-    tabIconDefault: '#687076',
-    tabIconSelected: tintColorLight,
+    white: "#FFFFFF",
+    light: "#F0EFEF",
+    lightGray: "#EEEEEE",
+    primaryLighter: "#5BBA6F",
+    primaryLight: "#179226",
+    primaryMain: "#2B7035",
+    primaryDark: "#137547",
+    primaryDarker: "#054A29",
+    error: "#FA1E00",
+    info: "#5BBA6F",
+    success: "#137547",
+    warning: "#F5FA00",
   },
   dark: {
-    text: '#ECEDEE',
-    background: '#151718',
-    tint: tintColorDark,
-    icon: '#9BA1A6',
-    tabIconDefault: '#9BA1A6',
-    tabIconSelected: tintColorDark,
+    white: "#FFFFFF",
+    light: "#F0EFEF",
+    lightGray: "#EEEEEE",
+    primaryLighter: "#5BBA6F",
+    primaryLight: "#179226",
+    primaryMain: "#2B7035",
+    primaryDark: "#137547",
+    primaryDarker: "#054A29",
+    error: "#FA1E00",
+    info: "#5BBA6F",
+    success: "#137547",
+    warning: "#F5FA00",
   },
 };
