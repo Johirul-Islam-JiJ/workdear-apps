df97c205c9bc3804c6432fcf2d1bf9fc3eafd135
Md Iqbal Hossen <iqbalhossen60483@gmail.com>
2025-09-08 17:41:33 +0600

    stepper added

--- app/(mainLayout)/(tabs)/postJob.tsx
+++ app/(mainLayout)/(tabs)/postJob.tsx
@@ -1,11 +1,23 @@
 import { ThemedText } from "@/components/libs/ThemedText";
 import { ThemedView } from "@/components/libs/ThemedView";
+import Stepper from "@/components/postJob/Stepper";
 import React from "react";
 
 const PostJobScreen = () => {
   return (
-    <ThemedView>
-      <ThemedText>Post Job</ThemedText>
+    <ThemedView
+      color="lightGray"
+      style={{
+        flex: 1,
+        paddingHorizontal: 10,
+        paddingVertical: 15,
+        rowGap: 15,
+      }}
+    >
+      <Stepper />
+      <ThemedText type="defaultSemiBold" color="primaryDarker">
+        Select country you want to hide from the selected zone (optional)
+      </ThemedText>
     </ThemedView>
   );
 };
---
+++ components/postJob/Stepper.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { View } from "react-native";
+import { ThemedView } from "../libs/ThemedView";
+
+const Stepper = ({ selected = 0 }) => {
+  return (
+    <View style={{ flexDirection: "row", alignItems: "center" }}>
+      {Array.from({ length: 4 }, (_, index) => (
+        <React.Fragment key={index}>
+          <ThemedView
+            color={selected >= index ? "error" : "placeHolder"}
+            style={{ width: 20, height: 20, borderRadius: 100 }}
+          />
+          {index !== 3 && (
+            <ThemedView
+              color={selected > index ? "error" : "placeHolder"}
+              style={{
+                flex: 1,
+                height: 2,
+              }}
+            />
+          )}
+        </React.Fragment>
+      ))}
+    </View>
+  );
+};
+
+export default Stepper;
