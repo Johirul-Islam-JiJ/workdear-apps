a34129e756d0c8011ca327fd2b5164b902ebb878
Md Iqbal Hossen <iqbalhossen60483@gmail.com>
2025-09-08 14:41:01 +0600

    nav inside banner added

--- components/home/Banner.tsx
+++ components/home/Banner.tsx
@@ -1,3 +1,5 @@
+import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
+import Ionicons from "@expo/vector-icons/Ionicons";
 import { BlurView } from "expo-blur";
 import React, { useEffect, useState } from "react";
 import {
@@ -5,11 +7,13 @@ import {
   Dimensions,
   Easing,
   ImageBackground,
+  Pressable,
   StyleSheet,
   View,
 } from "react-native";
 import Button from "../libs/Button";
 import { ThemedText } from "../libs/ThemedText";
+import { ThemedView } from "../libs/ThemedView";
 
 const { width } = Dimensions.get("window");
 
@@ -77,6 +81,28 @@ const Banner = () => {
     >
       <BlurView intensity={50} style={styles.blur}>
         <View style={styles.overlay} />
+        <View style={styles.navBar}>
+          <Pressable>
+            <FontAwesome6 name="bars" size={24} color="white" />
+          </Pressable>
+
+          <View style={styles.balanceContainer}>
+            <ThemedView color="primaryDarker" style={styles.balance}>
+              <ThemedText color="white" type="defaultSemiBold">
+                $9800.9999
+              </ThemedText>
+            </ThemedView>
+            <ThemedView color="primaryLight" style={styles.balance}>
+              <ThemedText color="white" type="defaultSemiBold">
+                $9800.9999
+              </ThemedText>
+            </ThemedView>
+            <View style={{ position: "relative" }}>
+              <Ionicons name="notifications-sharp" size={24} color="yellow" />
+              <ThemedView color="primaryLight" style={styles.notification} />
+            </View>
+          </View>
+        </View>
         <Animated.View style={[styles.content, { opacity: fadeAnim }]}>
           <ThemedText type="subtitle" color="white">
             {heroData[currentSlide].title}
@@ -99,8 +125,6 @@ const styles = StyleSheet.create({
   },
   blur: {
     ...StyleSheet.absoluteFillObject,
-    justifyContent: "center",
-    alignItems: "center",
   },
   overlay: {
     ...StyleSheet.absoluteFillObject,
@@ -110,6 +134,33 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
     zIndex: 2,
     rowGap: 10,
+  },
+  navBar: {
+    paddingHorizontal: 10,
+    paddingVertical: 8,
+    marginBottom: 10,
+    zIndex: 2,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  balanceContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 3,
+  },
+  balance: {
+    paddingHorizontal: 20,
+    paddingVertical: 5,
+    borderRadius: 100,
+  },
+  notification: {
+    position: "absolute",
+    top: 0,
+    right: 0,
+    width: 10,
+    height: 10,
+    borderRadius: 100,
   },
 });
 
