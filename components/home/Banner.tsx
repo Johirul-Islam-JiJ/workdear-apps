520d1df6eff6eb2875fec857668e19b8e589c831
Md Iqbal Hossen <iqbalhossen60483@gmail.com>
2025-09-08 13:40:43 +0600

    banner slider added

--- components/home/Banner.tsx
+++ components/home/Banner.tsx
@@ -1,31 +1,92 @@
 import { BlurView } from "expo-blur";
-import React from "react";
-import { ImageBackground, StyleSheet, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import {
+  Animated,
+  Dimensions,
+  Easing,
+  ImageBackground,
+  StyleSheet,
+  View,
+} from "react-native";
 import Button from "../libs/Button";
 import { ThemedText } from "../libs/ThemedText";
 
+const { width } = Dimensions.get("window");
+
+const heroData = [
+  {
+    id: 1,
+    title: "Best Microjob & Freelancing Site to Make Money Online",
+    buttonText: "Start earning now",
+    image: require("../../assets/images/banner-1.png"),
+  },
+  {
+    id: 2,
+    title: "Turn Your Skills Into Income, Join the Top Microjob Platform",
+    buttonText: "Start Your First Job",
+    image: require("../../assets/images/banner-2.jpg"),
+  },
+  {
+    id: 3,
+    title: "Work Anytime, Anywhere Earn Online with Microjobs",
+    buttonText: "Begin Your Journey",
+    image: require("../../assets/images/banner-3.jpg"),
+  },
+];
+
 const Banner = () => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const [fadeAnim] = useState(new Animated.Value(0));
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      // fade out
+      Animated.timing(fadeAnim, {
+        toValue: 0,
+        duration: 600,
+        useNativeDriver: true,
+      }).start(() => {
+        // switch slide
+        setCurrentSlide((prev) => (prev + 1) % heroData.length);
+
+        // fade in
+        Animated.timing(fadeAnim, {
+          toValue: 1,
+          duration: 600,
+          useNativeDriver: true,
+          easing: Easing.out(Easing.ease),
+        }).start();
+      });
+    }, 5000);
+
+    // initial fade in
+    Animated.timing(fadeAnim, {
+      toValue: 1,
+      duration: 800,
+      useNativeDriver: true,
+    }).start();
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <ImageBackground
-      source={require("../../assets/images/banner-1.png")}
+      source={heroData[currentSlide].image}
       style={styles.background}
       resizeMode="cover"
     >
-      {/* Blur effect */}
       <BlurView intensity={50} style={styles.blur}>
-        {/* Dark overlay */}
         <View style={styles.overlay} />
-
-        <View style={styles.content}>
+        <Animated.View style={[styles.content, { opacity: fadeAnim }]}>
           <ThemedText type="subtitle" color="white">
-            Work Anytime, Anywhere Earn Online with Microjobs
+            {heroData[currentSlide].title}
           </ThemedText>
           <Button
-            title="Begin Your Journey"
+            title={heroData[currentSlide].buttonText}
             color="primaryLight"
             style={{ width: 200 }}
           />
-        </View>
+        </Animated.View>
       </BlurView>
     </ImageBackground>
   );
@@ -33,7 +94,7 @@ const Banner = () => {
 
 const styles = StyleSheet.create({
   background: {
-    width: "100%",
+    width: width,
     height: 200,
   },
   blur: {
@@ -49,25 +110,6 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
     zIndex: 2,
     rowGap: 10,
-  },
-  title: {
-    color: "#fff",
-    fontSize: 24,
-    fontWeight: "bold",
-    textAlign: "center",
-  },
-  button: {
-    marginTop: 20,
-    backgroundColor: "green",
-    paddingHorizontal: 20,
-    paddingVertical: 10,
-    borderRadius: 8,
-    alignSelf: "center",
-  },
-  buttonText: {
-    color: "#fff",
-    fontSize: 16,
-    fontWeight: "bold",
   },
 });
 
