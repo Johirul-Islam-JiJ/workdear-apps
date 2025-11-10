f2aeb5de0ee995eaa617b5407df02c72436ad316
Md Iqbal Hossen <iqbalhossen60483@gmail.com>
2025-11-10 13:30:46 +0600

    fix: api key fixed

--- config/config.ts
+++ config/config.ts
@@ -2,7 +2,7 @@ export const config = {
   apiBaseUrl: process.env.EXPO_PUBLIC_API_BASE_URL,
   fileBaseUrl: process.env.EXPO_PUBLIC_API_FILE_URL,
   dataLimit: process.env.EXPO_PUBLIC_DATA_LIMIT,
-  apiKey: process.env.EXPO_PUBLIC_API_BASE_URL,
+  apiKey: process.env.EXPO_PUBLIC_API_KEY,
   liveSupportApiBaseUrl: process.env.EXPO_PUBLIC_LIVE_SUPPORT_API_BASE_URL,
   socketUrl: process.env.EXPO_PUBLIC_LIVE_SUPPORT_SERVER_URL,
   liveSupportServerUrl: process.env.EXPO_PUBLIC_SOCKET_URL,
