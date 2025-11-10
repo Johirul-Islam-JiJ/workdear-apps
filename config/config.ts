8eb3a818bb9cdf49086b0ada7c17af5bfdeb5328
Md Iqbal Hossen <iqbalhossen60483@gmail.com>
2025-11-10 13:27:30 +0600

    add: env added and all secrate moved there

--- config/config.ts
+++ config/config.ts
@@ -1,10 +1,10 @@
 export const config = {
-  apiBaseUrl: "https://backend.workdear.com/api",
-  fileBaseUrl: "https://backend.workdear.com/public/storage/",
-  dataLimit: 10,
-  apiKey: "wNYxWEWlkHN6Rz42yJxaMQbzA8d4Rsmyuwa/3N202mU=",
-  liveSupportApiBaseUrl: "https://support-server-t0yb.onrender.com/api",
-  socketUrl: "ws://support-server-t0yb.onrender.com/live-support",
-  liveSupportServerUrl: "https://support-server-t0yb.onrender.com",
-  geolocationApiKey: "AIzaSyA2y9j0Rd1I1ynHqRphdWDAi7fDfzAolAU",
+  apiBaseUrl: process.env.EXPO_PUBLIC_API_BASE_URL,
+  fileBaseUrl: process.env.EXPO_PUBLIC_API_FILE_URL,
+  dataLimit: process.env.EXPO_PUBLIC_DATA_LIMIT,
+  apiKey: process.env.EXPO_PUBLIC_API_BASE_URL,
+  liveSupportApiBaseUrl: process.env.EXPO_PUBLIC_LIVE_SUPPORT_API_BASE_URL,
+  socketUrl: process.env.EXPO_PUBLIC_LIVE_SUPPORT_SERVER_URL,
+  liveSupportServerUrl: process.env.EXPO_PUBLIC_SOCKET_URL,
+  geolocationApiKey: process.env.EXPO_PUBLIC_GEOLOCATION_API_KEY,
 };
