46b357594ca78c5aeb829df60d0328df9dc2c05d
Md Iqbal Hossen <iqbalhossen60483@gmail.com>
2025-11-11 18:42:32 +0600

    improve: content renderer component performance improved

--- components/libs/ContentRenderer.tsx
+++ components/libs/ContentRenderer.tsx
@@ -1,5 +1,5 @@
 import { useThemeColor } from "@/hooks/useThemeColor";
-import React from "react";
+import React, { useMemo } from "react";
 import { useWindowDimensions } from "react-native";
 import RenderHTML, { MixedStyleRecord } from "react-native-render-html";
 
@@ -7,49 +7,50 @@ export default function ContentRenderer({ html }: { html: string }) {
   const { width } = useWindowDimensions();
   const textColor = useThemeColor("text");
 
-  const tagsStyles: MixedStyleRecord = {
-    p: {
-      fontSize: 16,
-      lineHeight: 24,
-      marginBottom: 8,
-      color: textColor,
-    },
-    strong: {
-      fontWeight: "bold",
-      color: textColor,
-    },
-    h1: {
-      fontSize: 26,
-      fontWeight: "700",
-      marginBottom: 12,
-      color: textColor,
-    },
-    h2: {
-      fontSize: 22,
-      fontWeight: "600",
-      marginBottom: 10,
-      color: textColor,
-    },
-    a: {
-      textDecorationLine: "underline",
-      color: textColor,
-    },
-    ul: {
-      marginLeft: 16,
-      marginBottom: 10,
-      color: textColor,
-    },
-    li: {
-      fontSize: 16,
-      color: textColor,
-    },
-  };
+  const tagsStyles = useMemo<MixedStyleRecord>(
+    () => ({
+      p: {
+        fontSize: 16,
+        lineHeight: 24,
+        marginBottom: 8,
+        color: textColor,
+      },
+      strong: {
+        fontWeight: "bold",
+        color: textColor,
+      },
+      h1: {
+        fontSize: 26,
+        fontWeight: "700",
+        marginBottom: 12,
+        color: textColor,
+      },
+      h2: {
+        fontSize: 22,
+        fontWeight: "600",
+        marginBottom: 10,
+        color: textColor,
+      },
+      a: {
+        textDecorationLine: "underline",
+        color: textColor,
+      },
+      ul: {
+        marginLeft: 16,
+        marginBottom: 10,
+        color: textColor,
+      },
+      li: {
+        fontSize: 16,
+        color: textColor,
+      },
+    }),
+    [textColor]
+  );
+
+  const source = useMemo(() => ({ html }), [html]);
 
   return (
-    <RenderHTML
-      contentWidth={width}
-      source={{ html }}
-      tagsStyles={tagsStyles}
-    />
+    <RenderHTML contentWidth={width} source={source} tagsStyles={tagsStyles} />
   );
 }
