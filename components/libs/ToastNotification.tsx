0ddea232fe5056ff48854fc9d596d518d3f95f31
Md Iqbal Hossen <iqbalhossen60483@gmail.com>
2025-11-12 18:50:43 +0600

    improve: toast notification improved

--- components/libs/ToastNotification.tsx
+++ components/libs/ToastNotification.tsx
@@ -1,39 +1,40 @@
+import { ColorScheme } from "@/constants/Colors";
 import { useAppDispatch, useAppSelector } from "@/hooks/redux";
 import { useThemeColor } from "@/hooks/useThemeColor";
 import { hideNotification } from "@/store/slices/notification";
-import AntDesign from "@expo/vector-icons/AntDesign";
 import Entypo from "@expo/vector-icons/Entypo";
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 import React, { useEffect } from "react";
-import { Animated } from "react-native";
+import { Animated, ViewStyle } from "react-native";
+import AppIcon from "./AppIcon";
 import { ThemedText } from "./ThemedText";
 
 const ToastNotification = () => {
+  const backgroundColor = useThemeColor("card");
   const dispatch = useAppDispatch();
   const { visible, message, type } = useAppSelector(
     (state) => state.notification
   );
 
-  const textColor = useThemeColor(
-    type === "error" ? "error" : type === "success" ? "primarydark" : "text"
-  );
+  const textColor: ColorScheme =
+    type === "error" ? "error" : type === "success" ? "success" : "text";
   const opacity = new Animated.Value(0);
 
   useEffect(() => {
     if (visible) {
       Animated.timing(opacity, {
         toValue: 1,
-        duration: 300,
+        duration: 200,
         useNativeDriver: true,
       }).start();
 
       const timer = setTimeout(() => {
         Animated.timing(opacity, {
           toValue: 0,
-          duration: 300,
+          duration: 100,
           useNativeDriver: true,
         }).start(() => dispatch(hideNotification()));
-      }, 3000);
+      }, 2000);
 
       return () => clearTimeout(timer);
     }
@@ -41,33 +42,38 @@ const ToastNotification = () => {
 
   if (!visible) return null;
 
+  const containerStyle: ViewStyle = {
+    position: "absolute",
+    bottom: 85,
+    left: "10%",
+    right: "10%",
+    paddingHorizontal: 10,
+    paddingVertical: 8,
+    borderRadius: 10,
+    backgroundColor,
+    opacity,
+    elevation: 1,
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 10,
+  };
+
   return (
-    <Animated.View
-      style={{
-        position: "absolute",
-        top: "50%",
-        left: "10%",
-        right: "10%",
-        transform: [{ translateY: "-50%" }],
-        padding: 15,
-        height: 200,
-        borderRadius: 10,
-        backgroundColor: "white",
-        opacity,
-        elevation: 5,
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+    <Animated.View style={containerStyle}>
       {type === "success" ? (
-        <AntDesign name="check-circle" size={50} color={textColor} />
+        <AppIcon color="success">
+          <MaterialIcons name="check-circle" />
+        </AppIcon>
       ) : type === "error" ? (
-        <MaterialIcons name="error" size={50} color={textColor} />
+        <AppIcon color="error">
+          <MaterialIcons name="error" />
+        </AppIcon>
       ) : (
-        <Entypo name="info-with-circle" size={50} color={textColor} />
+        <AppIcon color="info">
+          <Entypo name="info-with-circle" />
+        </AppIcon>
       )}
-      <ThemedText style={{ color: textColor, textAlign: "center" }}>
+      <ThemedText color={textColor} style={{ width: "90%", fontSize: 18 }}>
         {message}
       </ThemedText>
     </Animated.View>
