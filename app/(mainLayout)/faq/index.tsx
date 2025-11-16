2c62f1e2fcc2c7cf158e9af5cbf1bc8b5a634e86
Md Iqbal Hossen <iqbalhossen60483@gmail.com>
2025-11-16 17:29:11 +0600

    add: faq screen added

--- app/(mainLayout)/faq/index.tsx
+++ app/(mainLayout)/faq/index.tsx
@@ -1,8 +1,25 @@
+import FaqList from "@/components/home/FaqList";
+import { ThemedView } from "@/components/libs/ThemedView";
+import { useGetFaqContentQuery } from "@/store/features/content";
+import { Faq } from "@/types/Faq";
 import React from "react";
-import { View } from "react-native";
+import { ScrollView, View } from "react-native";
 
-const Index = () => {
-  return <View></View>;
+const FaqScreen = () => {
+  const { data } = useGetFaqContentQuery();
+  const faqData: Faq[] = data?.faq_data ?? [];
+
+  return (
+    <ThemedView color="background" style={{ flex: 1 }}>
+      <ScrollView style={{ flex: 1 }}>
+        <View style={{ rowGap: 8, padding: 10 }}>
+          {faqData.map((item, index) => (
+            <FaqList key={index} item={item} />
+          ))}
+        </View>
+      </ScrollView>
+    </ThemedView>
+  );
 };
 
-export default Index;
+export default FaqScreen;
