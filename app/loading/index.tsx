fe58c097b9f3977036b4a35465f50cff744741c4
Md Iqbal Hossen <iqbalhossen60483@gmail.com>
2025-12-03 11:56:51 +0600

    android build added

--- app/loading/index.tsx
+++ app/loading/index.tsx
@@ -1,14 +1,13 @@
 import LoadingIndicator from "@/components/libs/LoadingIndicator";
 import { ThemedView } from "@/components/libs/ThemedView";
 import React from "react";
-import { View } from "react-native";
 
 const loading = () => {
   return (
-    <View style={{ flex: 1 }}>
+    <ThemedView color="background" style={{ flex: 1 }}>
       <ThemedView color="primarydark" style={{ height: 35 }} />
       <LoadingIndicator fullScreen />
-    </View>
+    </ThemedView>
   );
 };
 
