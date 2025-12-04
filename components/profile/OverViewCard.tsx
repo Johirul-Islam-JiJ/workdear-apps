f5ab1918d023c8d3f965068867e91b959f52a3cc
Md Iqbal Hossen <iqbalhossen60483@gmail.com>
2025-12-04 16:29:59 +0600

    overview card added

---
+++ components/profile/OverViewCard.tsx
@@ -0,0 +1,66 @@
+import React from "react";
+import { View } from "react-native";
+import Card from "../libs/Card";
+import Divider from "../libs/Divider";
+import Rating from "../libs/Rating";
+import { ThemedText } from "../libs/ThemedText";
+
+type Props = {
+  data: {
+    title: string;
+    tag: string;
+    rating: number;
+    works: {
+      title: string;
+      value: number;
+    }[];
+  };
+};
+
+const OverViewCard = ({ data }: Props) => {
+  return (
+    <Card>
+      <View>
+        <ThemedText
+          variant="subtitle"
+          style={{ textAlign: "center" }}
+          color="primarymain"
+        >
+          {data.title}
+        </ThemedText>
+        <ThemedText variant="small" style={{ textAlign: "center" }}>
+          {data.tag}
+        </ThemedText>
+      </View>
+      <View>
+        {data.works.map((work, index) => (
+          <View key={work.title}>
+            <View
+              style={{
+                flexDirection: "row",
+                justifyContent: "space-between",
+                marginVertical: 5,
+              }}
+            >
+              <ThemedText>{work.title}</ThemedText>
+              <ThemedText>{work.value}</ThemedText>
+            </View>
+            {data.works.length - 1 !== index && <Divider />}
+          </View>
+        ))}
+      </View>
+      <View style={{ alignItems: "center" }}>
+        <Rating value={data.rating} />
+        <ThemedText variant="small">
+          {data.rating >= 4
+            ? "Excellent"
+            : data.rating <= 3 && data.rating > 1
+            ? "Good"
+            : "Not Rated yet"}
+        </ThemedText>
+      </View>
+    </Card>
+  );
+};
+
+export default OverViewCard;
--- components/profile/WorkingOverview.tsx
+++ components/profile/WorkingOverview.tsx
@@ -1,5 +1,6 @@
 import { useAppSelector } from "@/hooks/redux";
 import React from "react";
+import OverViewCard from "./OverViewCard";
 import StatusCard from "./StatusCard";
 
 const WorkingOverview = () => {
@@ -14,9 +15,38 @@ const WorkingOverview = () => {
     pending: user_rating?.pending_percentage,
   } as any;
 
+  const overviewContent = {
+    title: "Overview",
+    tag: "Total Working",
+    rating: user_rating?.star_count,
+    works: [
+      {
+        title: "Task Attend",
+        value: user_rating?.total_submissions,
+      },
+      {
+        title: "Satisfied",
+        value: user_rating?.satisfied_count,
+      },
+      {
+        title: "Not Satisfied",
+        value: user_rating?.unsatisfied_count,
+      },
+      {
+        title: "Pending",
+        value: user_rating?.pending,
+      },
+      {
+        title: "Total Earn",
+        value: `$${user?.user_transaction_record?.job_earn}` || "_",
+      },
+    ],
+  } as any;
+
   return (
     <>
       <StatusCard data={statusContent} />
+      <OverViewCard data={overviewContent} />
     </>
   );
 };
