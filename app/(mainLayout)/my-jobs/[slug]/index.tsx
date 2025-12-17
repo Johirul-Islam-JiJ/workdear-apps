bc7442387ca5aaa3848fbb780a9a4fafdb893ba1
Md Iqbal Hossen <iqbalhossen60483@gmail.com>
2025-12-17 17:33:01 +0600

    my job detail screen added

--- app/(mainLayout)/my-jobs/[slug]/index.tsx
+++ app/(mainLayout)/my-jobs/[slug]/index.tsx
@@ -1,11 +1,14 @@
-import { ThemedText } from "@/components/libs/ThemedText";
 import { ThemedView } from "@/components/libs/ThemedView";
+import MyJobDetailsContent from "@/components/my-job/details/MyJobDetailsContent";
 import React from "react";
+import { ScrollView } from "react-native";
 
 const MyJobDetails = () => {
   return (
-    <ThemedView color="background">
-      <ThemedText>My Jobs</ThemedText>
+    <ThemedView color="background" style={{ flex: 1 }}>
+      <ScrollView style={{ flex: 1 }}>
+        <MyJobDetailsContent />
+      </ScrollView>
     </ThemedView>
   );
 };
