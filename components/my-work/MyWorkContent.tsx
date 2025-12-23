2dd95d82f6b2908aa343e727cb4405becdc7576c
Md Iqbal Hossen <iqbalhossen60483@gmail.com>
2025-12-23 11:42:27 +0600

    inside my work no work found added

--- components/my-work/MyWorkContent.tsx
+++ components/my-work/MyWorkContent.tsx
@@ -1,9 +1,12 @@
 import { useGetTasksQuery } from "@/store/features/my-work";
 import { MyWork, MyWorkSummaryType } from "@/types/myWork";
+import { Link } from "expo-router";
 import React, { useState } from "react";
 import { Dimensions, View } from "react-native";
+import Button from "../libs/Button";
 import LoadingIndicator from "../libs/LoadingIndicator";
 import Pagination from "../libs/Pagination";
+import { ThemedText } from "../libs/ThemedText";
 import MyWorkList from "./MyWorkList";
 import MyWorkSummary from "./MyWorkSummary";
 
@@ -17,7 +20,7 @@ const MyWorkContent = () => {
   const totalPages: number = data?.last_page || 1;
   const workSummary: MyWorkSummaryType = response?.data?.statistics || {};
 
-  if (isLoading)
+  if (isLoading) {
     return (
       <LoadingIndicator
         fullScreen
@@ -26,6 +29,29 @@ const MyWorkContent = () => {
         }}
       />
     );
+  }
+
+  if (tasks.length === 0) {
+    return (
+      <View
+        style={{
+          height: Dimensions.get("screen").height - 140,
+          justifyContent: "center",
+          alignItems: "center",
+          rowGap: 5,
+        }}
+      >
+        <ThemedText>No work found</ThemedText>
+        <Link href="/(mainLayout)/(drawer)/(tabs)/jobs" asChild>
+          <Button
+            title="Find your first work"
+            size="small"
+            endIcon="arrow-up-right-box-outline"
+          />
+        </Link>
+      </View>
+    );
+  }
 
   return (
     <View style={{ rowGap: 20, padding: 10 }}>
