e60a39299364067553ea99877d4c35098efe122b
Md Iqbal Hossen <iqbalhossen60483@gmail.com>
2025-12-23 11:36:05 +0600

    no job component added

--- components/my-job/MyJobContent.tsx
+++ components/my-job/MyJobContent.tsx
@@ -1,9 +1,12 @@
 import { useMyJobsData } from "@/hooks/useMyJobsData";
 import { MyJob, MyJobStatistics } from "@/types/myJobs";
+import { Link } from "expo-router";
 import React from "react";
 import { Dimensions, View } from "react-native";
+import Button from "../libs/Button";
 import LoadingIndicator from "../libs/LoadingIndicator";
 import Pagination from "../libs/Pagination";
+import { ThemedText } from "../libs/ThemedText";
 import FilterAndHeader from "./FilterAndHeader";
 import MyJobSummary from "./MyJobSummary";
 import MyjobsList from "./MyjobsList";
@@ -24,6 +27,28 @@ const MyJobContent = () => {
         }}
       />
     );
+
+  if (jobs.length === 0) {
+    return (
+      <View
+        style={{
+          height: Dimensions.get("screen").height - 140,
+          justifyContent: "center",
+          alignItems: "center",
+          rowGap: 5,
+        }}
+      >
+        <ThemedText>No job found</ThemedText>
+        <Link href="/(mainLayout)/(drawer)/(tabs)/post-job" asChild>
+          <Button
+            title="Add your first job"
+            size="small"
+            endIcon="arrow-up-right-box-outline"
+          />
+        </Link>
+      </View>
+    );
+  }
 
   return (
     <View style={{ padding: 10, rowGap: 10 }}>
