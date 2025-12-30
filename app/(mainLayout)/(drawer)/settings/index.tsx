e263634859f76e1f929bff0aa522af95731695f7
Md Iqbal Hossen <iqbalhossen60483@gmail.com>
2025-12-30 18:04:18 +0600

    setting screen keyboard avoiding fixed

--- app/(mainLayout)/(drawer)/settings/index.tsx
+++ app/(mainLayout)/(drawer)/settings/index.tsx
@@ -2,20 +2,38 @@ import Container from "@/components/common/Container";
 import AccountDeactivation from "@/components/setting/AccountDeactivation";
 import AccountPassword from "@/components/setting/AccountPassword";
 import EditProfile from "@/components/setting/EditProfile";
-import React from "react";
-import { View } from "react-native";
-import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import React, { useEffect, useState } from "react";
+import { Keyboard, View } from "react-native";
 
 const Settings = () => {
+  const [keyboardHeight, setKeyboardHeight] = useState(0);
+
+  useEffect(() => {
+    const keyboardDidShowListener = Keyboard.addListener(
+      "keyboardDidShow",
+      (e) => {
+        setKeyboardHeight(e.endCoordinates.height);
+      }
+    );
+    const keyboardDidHideListener = Keyboard.addListener(
+      "keyboardDidHide",
+      () => {
+        setKeyboardHeight(0);
+      }
+    );
+    return () => {
+      keyboardDidShowListener.remove();
+      keyboardDidHideListener.remove();
+    };
+  }, []);
+
   return (
     <Container>
-      <KeyboardAwareScrollView>
-        <View style={{ rowGap: 10 }}>
-          <EditProfile />
-          <AccountPassword />
-          <AccountDeactivation />
-        </View>
-      </KeyboardAwareScrollView>
+      <View style={{ rowGap: 10, marginBottom: keyboardHeight }}>
+        <EditProfile />
+        <AccountPassword />
+        <AccountDeactivation />
+      </View>
     </Container>
   );
 };
